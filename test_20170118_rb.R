d0f72428587a0e1773b534a439bc281748ed4811
Ryan <rybyrnes@gmail.com>
2018-01-18 14:45:35 -0800

    Function to import .hru into R, not working though.
    URL: http://santiago.begueria.es/2013/10/reading-swat-output-hru-files-in-r/#comment-24830

--- test_20170118_rb.R
+++ test_20170118_rb.R
@@ -9,7 +9,11 @@ library("ggplot2")
 # Set Directory Path #
 swatpath <- "C:/Users/rbyrnes/Desktop/SWAT R Analysis/SWAT Output" 
 
-# Read in data as text #
+# Read in HRU.text data #
 swat.output.text <- read.table(file.path(swatpath, "hru2.txt"), header = TRUE, fill = TRUE, sep=',') 
+
+# Read in data as an HRU file #
+file <- 'C:/Users/rbyrnes/Desktop/SWAT R Analysis/SWAT Output/output2.hru'
+swat.hru.test <- swat_readOutputhru(file, col=w,hru = 2, ver=2012)
 
 # Summarize data using doBy package and summaryBy() #
