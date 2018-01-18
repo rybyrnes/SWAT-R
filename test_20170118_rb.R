53e3bd60991b998f450dd26d1583b0a965c8250d
Ryan <rybyrnes@gmail.com>
2018-01-18 10:26:14 -0800

    Figured out how to import SWAT text output instead of CSV and ensured packages would be installed if not already present on machine

--- test_20170118_rb.R
+++ test_20170118_rb.R
@@ -1,3 +1,7 @@
+# Install doBy and ggplot2 #
+install.packages("doBy")
+install.packages("ggplot2")
+
 # Load Packages doBy for summarizing statistics and ggplot2 for plotting #
 library("doBy")
 library("ggplot2")
@@ -5,7 +9,7 @@ library("ggplot2")
 # Set Directory Path #
 swatpath <- "C:/Users/rbyrnes/Desktop/SWAT R Analysis/SWAT Output" 
 
-# Read in data #
-swat.output.test <- read.csv(file.path(swatpath, "hru.output.csv"), header = TRUE) 
+# Read in data as text #
+swat.output.text <- read.table(file.path(swatpath, "hru2.txt"), header = TRUE, fill = TRUE, sep=',') 
 
 # Summarize data using doBy package and summaryBy() #
